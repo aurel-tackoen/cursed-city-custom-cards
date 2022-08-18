af7638175990908506bedbef95e13f49d1a4f262
aurel-tackoen <aurel.tackoen@opengraphy.com>
2022-08-19 01:23:43 +0200

    feat: mongodb

--- src/functions/User.js
+++ src/functions/User.js
@@ -1,8 +1,19 @@
+const { MongoClient } = require('mongodb');
+
+const mongoClient = new MongoClient(process.env.MONGODB_URI);
+
+const clientPromise = mongoClient.connect();
+
 exports.handler = async function (event, context) {
-  console.log(event);
-  console.log(context);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello World' }),
-  };
+  try {
+    const database = (await clientPromise).db('cursed-database');
+    const collection = database.collection('Users');
+    const results = await collection.find({}).limit(10).toArray();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(results),
+    };
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() };
+  }
 };
