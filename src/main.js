abd294d69868f0bae146ebf733f6bb7a2c88f34c
aurel-tackoen <aurel.tackoen@opengraphy.com>
2022-08-19 14:58:32 +0200

    netlify login

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import FontAwesomeIcon from '@/services/fontawesome.js';
+import FontAwesomeIcon from '@/plugins/fontawesome.js';
 
 import App from './App.vue';
 import router from './router';
