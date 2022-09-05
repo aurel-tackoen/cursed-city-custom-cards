bc6942c3b9f1ad55984423c04e2d15deda6c743b
aurel-tackoen <aurel@opengraphy.com>
2022-09-05 17:49:30 +0200

    feat: align icons

--- src/plugins/markdown.js
+++ src/plugins/markdown.js
@@ -5,11 +5,17 @@ import DiceD12 from '@/assets/svg/dice-d12.svg?raw';
 
 function generateHtml(text) {
   if (text) {
-    text = text.replace('[D6]', `<div class="inline h-3 w-3">${DiceD6}</div>`);
-    text = text.replace('[D8]', `<div class="inline h-3 w-3">${DiceD8}</div>`);
+    text = text.replace(
+      '[D6]',
+      `<div class="inline align-text-top">${DiceD6}</div>`
+    );
+    text = text.replace(
+      '[D8]',
+      `<div class="inline align-text-top">${DiceD8}</div>`
+    );
     text = text.replace(
       '[D12]',
-      `<div class="inline h-3 w-3">${DiceD12}</div>`
+      `<div class="inline align-text-top">${DiceD12}</div>`
     );
     text = marked.parseInline(text);
     return text;
