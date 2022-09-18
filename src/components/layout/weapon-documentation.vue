3534f3beaf4c6f83b3eb8f53796c9d0d43ea009e
aurel-tackoen <aurel.tackoen@opengraphy.com>
2022-09-18 20:44:14 +0200

    feat: weapon note documentation

---
+++ src/components/layout/weapon-documentation.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue';
+</script>
+<template>
+  <Popover>
+    <PopoverButton>
+      <fa-icon
+        class="fa-fw text-slate-500 hover:text-red-700"
+        :icon="['fas', 'hat-wizard']"
+        title="Help"
+      />
+    </PopoverButton>
+    <PopoverPanel class="absolute z-10 -mt-8 w-96 -translate-x-96">
+      <div
+        class="y overflow-hidden rounded-lg border bg-white p-4 text-left shadow-lg"
+      >
+        If you want to add note to a weapon, first create the weapon, then the
+        note in the corresponding panel. After that you'll be able so select the
+        note in the weapon panel.
+      </div>
+    </PopoverPanel>
+  </Popover>
+</template>
