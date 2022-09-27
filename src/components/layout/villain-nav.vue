57a72f03af5c51df4c7783d8428b9ad254e4f244
aurel-tackoen <aurel@opengraphy.com>
2022-09-27 17:23:19 +0200

    feat: villain store and create

---
+++ src/components/layout/villain-nav.vue
@@ -0,0 +1,172 @@
+<script setup>
+  import { computed, inject } from 'vue';
+  const dayjs = inject('dayjs');
+  const props = defineProps({
+    villain: Object,
+    tabs: Array,
+    status: {
+      type: Boolean,
+      default: true,
+    },
+    save: {
+      type: Boolean,
+      default: false,
+    },
+    create: {
+      type: Boolean,
+      default: false,
+    },
+    print: {
+      type: Boolean,
+      default: false,
+    },
+    single: {
+      type: Boolean,
+      default: false,
+    },
+  });
+  const emit = defineEmits(['update:tabs', 'update:villain', 'create:villain']);
+  const tabs = computed({
+    get: () => props.tabs,
+    set: (value) => emit('update:tabs', value),
+  });
+  function setTabs(index) {
+    tabs.value.map((tab) => {
+      tab.current = false;
+    });
+    tabs.value[index].current = true;
+  }
+  function printVillain() {
+    window.print();
+  }
+</script>
+<template>
+  <div class="hidden-print sticky top-0 z-50 mx-auto">
+    <nav
+      class="flex h-20 w-full items-center overflow-hidden rounded bg-gradient-to-t from-slate-50 to-white px-2 shadow"
+      aria-label="Tabs"
+    >
+      <div class="flex w-7/12 items-center sm:w-5/12">
+        <div>
+          <div
+            class="flex items-center justify-center overflow-hidden rounded-full border shadow-inner"
+            style="width: 12mm; height: 12mm"
+          >
+            <img
+              v-if="villain.picture && villain.picture.url"
+              :src="villain.picture.url"
+              alt="Villain Picture"
+              class="max-w-max"
+              :style="`
+                  transform: scale(${villain.picture.zoom / 2});
+                  margin-top: ${villain.picture.offsetY / 2}px;
+                  margin-left: ${villain.picture.offsetX / 2}px;
+                  height: 40.7mm
+                `"
+            />
+            <fa-icon
+              v-else
+              class="fa-fw fa-xl mx-auto justify-self-center text-gray-400"
+              :icon="['fad', 'ghost']"
+            />
+          </div>
+        </div>
+        <div class="flex grow flex-col space-y-1 px-4">
+          <div class="text-lg font-bold leading-4 text-slate-900">
+            {{ villain.name }}
+          </div>
+          <div class="hidden text-xs italic leading-4 text-slate-600 sm:block">
+            Created by
+            <span class="font-bold">{{ villain.user.username }}</span>
+            {{ dayjs(villain.date * 1000).fromNow() }}
+          </div>
+        </div>
+      </div>
+      <div class="hidden justify-center self-stretch sm:flex sm:w-4/12">
+        <div v-if="status === true">
+          <button
+            v-for="(tab, tabIdx) in tabs"
+            class="relative h-full overflow-hidden px-3 text-center"
+            :key="tab.name"
+            :class="[
+              tab.current
+                ? 'cursor-default text-slate-900'
+                : 'text-slate-500 hover:text-red-700',
+            ]"
+            :aria-current="tab.current ? 'page' : undefined"
+            @click="setTabs(tabIdx)"
+          >
+            <span>{{ tab.name }}</span>
+            <span
+              aria-hidden="true"
+              :class="[
+                tab.current ? 'bg-red-700' : 'bg-transparent',
+                'absolute inset-x-0 bottom-0 h-1',
+              ]"
+            />
+          </button>
+        </div>
+      </div>
+      <div class="flex w-5/12 items-center justify-end space-x-2 sm:w-3/12">
+        <button
+          v-if="save === true"
+          @click="emit('update:villain')"
+          class="btn-primary"
+        >
+          Save
+        </button>
+        <button
+          v-if="create === true"
+          @click="emit('create:villain')"
+          class="btn-primary"
+        >
+          Create
+        </button>
+        <router-link
+          v-if="single === true"
+          :to="{ name: 'villains-single', params: { id: villain._id } }"
+          class="btn-primary"
+        >
+          View
+        </router-link>
+        <button
+          v-if="print === true"
+          @click="printVillain()"
+          class="btn-primary"
+        >
+          Print
+        </button>
+      </div>
+    </nav>
+    <nav
+      class="flex h-12 w-full items-center rounded bg-gradient-to-t from-slate-50 to-white px-2 shadow sm:hidden"
+      aria-label="Tabs"
+    >
+      <div class="flex w-full justify-center self-stretch sm:w-4/12">
+        <div v-if="status === true">
+          <button
+            v-for="(tab, tabIdx) in tabs"
+            class="relative h-full overflow-hidden px-3 text-center"
+            :key="tab.name"
+            :class="[
+              tab.current
+                ? 'cursor-default text-slate-900'
+                : 'text-slate-500 hover:text-red-700',
+            ]"
+            :aria-current="tab.current ? 'page' : undefined"
+            @click="setTabs(tabIdx)"
+          >
+            <span>{{ tab.name }}</span>
+            <span
+              aria-hidden="true"
+              :class="[
+                tab.current ? 'bg-red-700' : 'bg-transparent',
+                'absolute inset-x-0 bottom-0 h-1',
+              ]"
+            />
+          </button>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
--- src/pages/VillainCreatePage.vue
+++ src/pages/VillainCreatePage.vue
@@ -1,14 +1,55 @@
 <script setup>
-  import { reactive } from 'vue';
+  import { reactive, inject, onUnmounted } from 'vue';
+  import { storeToRefs } from 'pinia';
+  import { useRouter } from 'vue-router';
   import { cloneDeep } from 'lodash';
+  import { useAuthStore } from '@/stores/auth-store.js';
+  import { useVillainsStore } from '@/stores/villains-store.js';
   import { defaultVillain } from '@/assets/data/villain.js';
+
+  import VillainNav from '@/components/layout/villain-nav.vue';
   import VillainCard from '@/components/cards/villain-card.vue';
   import VillainForm from '@/components/cards/villain-form.vue';
+  import ErrorsAlert from '@/components/layout/errors-alert.vue';
+
+  const router = useRouter();
+  const dayjs = inject('dayjs');
   const newVillain = reactive(cloneDeep(defaultVillain));
+  const tabs = reactive([
+    { name: 'Normal', current: true },
+    { name: 'Empowered', current: false },
+  ]);
+
+  const authStore = useAuthStore();
+  const { User } = storeToRefs(authStore);
+  const villainsStore = useVillainsStore();
+  const { VillainErrors } = storeToRefs(villainsStore);
+
+  newVillain.user.email = User.value.email;
+  newVillain.user.username = User.value.username;
+  newVillain.date = dayjs().unix();
+
+  async function createVillain() {
+    const result = await villainsStore.createHero(newVillain);
+    if (result) {
+      router.push({ name: 'villains-update', params: { id: result._id } });
+      Object.assign(newVillain, defaultVillain);
+    }
+  }
+  onUnmounted(() => {
+    VillainErrors.value = [];
+  });
 </script>
 
 <template>
   <div class="mt-4 space-y-4">
+    <VillainNav
+      @create:villain="createVillain()"
+      :villain="newVillain"
+      v-model:tabs="tabs"
+      :create="true"
+    />
+    <ErrorsAlert :errors="VillainErrors" />
     <div
       class="flex flex-col justify-center space-y-2 lg:flex-row lg:space-x-2 lg:space-y-0"
     >
