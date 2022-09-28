caab7adf8c2f910b03ddf1259f475a50dcdbf545
aurel-tackoen <aurel@opengraphy.com>
2022-09-28 13:45:27 +0200

    feat: start villain form

--- src/components/cards/villain-form.vue
+++ src/components/cards/villain-form.vue
@@ -1,4 +1,6 @@
 <script setup>
+  import { computed, toRaw } from 'vue';
+  import { v4 as uuidv4 } from 'uuid';
   const props = defineProps({
     status: String,
     villain: {
@@ -12,7 +14,49 @@
       },
     },
   });
-  console.log(props);
+  const emit = defineEmits(['update:villain']);
+  const villain = computed({
+    get: () => props.villain,
+    set: (value) => emit('update:villain', value),
+  });
+  async function remove(target, index) {
+    if (confirm('Are you sure you want to delete this?') == true) {
+      villain.value[props.status][target].splice(index, 1);
+    }
+  }
+  const defaultWeapon = {
+    name: '',
+    type: 'melee',
+    dices: '',
+    damages: {
+      base: 1,
+      critical: 1,
+    },
+    notes: [],
+  };
+  const defaultNotes = {
+    name: '',
+    rule: '',
+  };
+  const defaultSpecial = {
+    name: '',
+    rule: '',
+  };
+  const defaultBehaviour = {
+    name: '',
+    rule: '',
+  };
+  function getError(path) {
+    return props?.errors.find((e) => e.path === path);
+  }
+  function copy(item) {
+    return structuredClone(toRaw(item));
+  }
+  function clone(item) {
+    if (confirm('Are you sure you want to copy this?') == true) {
+      return structuredClone(toRaw(item));
+    }
+  }
 </script>
 <template>
   {{ villain }}
