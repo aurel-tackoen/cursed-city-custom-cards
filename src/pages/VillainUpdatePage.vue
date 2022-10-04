987fbbf6702466a06ef5187868e34262f6ed5d8b
aurel-tackoen <aurel@opengraphy.com>
2022-10-04 16:57:04 +0200

    feat: villains update

--- src/pages/VillainUpdatePage.vue
+++ src/pages/VillainUpdatePage.vue
@@ -1,5 +1,94 @@
-<script setup></script>
+<script setup>
+  import { reactive, onUnmounted } from 'vue';
+  import { useRoute, useRouter } from 'vue-router';
+  import { storeToRefs } from 'pinia';
+  import { useAuthStore } from '@/stores/auth-store.js';
+  import { useVillainsStore } from '@/stores/villains-store.js';
+
+  import VillainNav from '@/components/layout/villain-nav.vue';
+  import VillainCard from '@/components/cards/villain-card.vue';
+  import VillainForm from '@/components/cards/villain-form.vue';
+  import ErrorsAlert from '@/components/layout/errors-alert.vue';
+
+  const route = useRoute();
+  const router = useRouter();
+  const authStore = useAuthStore();
+  const { User } = storeToRefs(authStore);
+  const villainsStore = useVillainsStore();
+  await villainsStore.fetchVillain(route.params.id);
+  const { Villain, VillainErrors } = storeToRefs(villainsStore);
+
+  function updateVillain() {
+    villainsStore.updateVillain();
+  }
+  async function removeVillain() {
+    if (confirm('Are you sure you want to delete this villain?') == true) {
+      await villainsStore.removeVillain(Villain.value._id);
+      router.push({ name: 'home' });
+    }
+  }
+
+  onUnmounted(() => {
+    VillainErrors.value = [];
+  });
+
+  const tabs = reactive([
+    { name: 'Path to Glory', current: true },
+    { name: 'Empowered', current: false },
+  ]);
+</script>
 
 <template>
-  <div class="mt-4 space-y-4">aaa</div>
+  <div class="mt-4 space-y-4" v-if="Villain">
+    <div v-if="User.email !== Villain.user.email" class="alert-container">
+      You are not allowed to update this Villain.
+    </div>
+    <VillainNav
+      @update:villain="updateVillain"
+      :villain="Villain"
+      v-model:tabs="tabs"
+      :save="User.email === Villain.user.email ? true : false"
+      :single="true"
+    />
+    <ErrorsAlert :errors="VillainErrors" />
+    <div
+      class="flex flex-col justify-center space-y-2 lg:flex-row lg:space-x-2 lg:space-y-0"
+    >
+      <VillainCard
+        v-if="tabs[0].current"
+        status="normal"
+        v-model:villain="Villain"
+        class="villain-card-display"
+      />
+      <VillainCard
+        v-if="tabs[1].current"
+        status="empowered"
+        v-model:villain="Villain"
+        class="villain-card-display"
+      />
+    </div>
+    <VillainForm
+      v-if="User.email === Villain.user.email && tabs[0].current"
+      status="normal"
+      v-model:villain="Villain"
+      :errors="VillainErrors"
+    />
+    <VillainForm
+      v-if="User.email === Villain.user.email && tabs[1].current"
+      status="empowered"
+      v-model:villain="Villain"
+      :errors="VillainErrors"
+    />
+    <div
+      v-if="User.email === Villain.user.email"
+      class="flex w-full items-center justify-start border-t pt-2"
+    >
+      <button
+        class="cursor-pointer text-red-500 hover:text-red-700 hover:underline"
+        @click="removeVillain"
+      >
+        Delete this villain
+      </button>
+    </div>
+  </div>
 </template>
